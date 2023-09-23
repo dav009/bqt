a09f7de99c24dc60803f2d704465f2bf3c2d548f
David Przybilla <dav.alejandro@gmail.com>
2023-09-23 21:16:50 +0900

    better gobuildmodule

--- flake.nix
+++ flake.nix
@@ -17,25 +17,26 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
-    in
-    {
+    in {
 
-      packages = forAllSystems
-        (system:
-          let
-            pkgs = nixpkgsFor.${system};
-          in rec {
-            bqt = pkgs.buildGoModule {
-              pname = "bqt";
-              inherit version;
-              src = ./.;
-              CGO_ENABLED = 1;
-              vendorSha256 =
-                "sha256-sjg+D0IIErl21HZjXBNKBTqXBZfy6w6EhHYS0seUE3k=";
-            };
-            default = bqt;
-          });
-
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in rec {
+          bqt = pkgs.buildGoModule {
+            pname = "bqt";
+            inherit version;
+            src = ./.;
+            CGO_ENABLED = 1;
+            preBuild = ''
+             export CXX = "clang++";
+             export CC = "clang";
+            '';
+            vendorSha256 =
+              "sha256-sjg+D0IIErl21HZjXBNKBTqXBZfy6w6EhHYS0seUE3k=";
+            nativeCheckInputs = with pkgs; [ clang_14 ];
+          };
+          default = bqt;
+        });
 
       apps = forAllSystems (system: rec {
         bqt = {
@@ -53,7 +54,14 @@
         let pkgs = nixpkgsFor.${system};
         in {
           default = pkgs.mkShell {
-            buildInputs = with pkgs; [ go_1_21 gopls gotools golint clang_14 goreleaser ];
+            buildInputs = with pkgs; [
+              go_1_21
+              gopls
+              gotools
+              golint
+              clang_14
+              goreleaser
+            ];
             GOROOT = "${pkgs.go_1_21}/share/go";
             CGO_ENABLED = "1";
             CXX = "clang++";
